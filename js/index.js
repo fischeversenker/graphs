34216bef8bffff08acf029b1fa4e8fe5fae0b6d0
Felix Hamann <felix.hamann@campus.lmu.de>
2018-02-01 02:41:20 +0100

    Fixed Labels

--- js/index.js
+++ js/index.js
@@ -9,96 +9,143 @@ Date.prototype.ddmmyyyy = function() {
   ].join('.');
 };
 
-class DivRenderer {
-  constructor(container) {
-    this.container = container;
-  }
-  draw(data) {
-    data.forEach(d => {
-      let el = document.createElement('div');
-      el.innerHTML = 'Datum: ' + d[0] + '<br>Value: ' + d[1] + '<hr>';
-      this.container.appendChild(el);
-    });
-  }
-}
-
 class CanvasRenderer {
-  constructor(canvas) {
+  constructor(canvas, options) {
+    this.config = {
+      padding: 40,
+      lineColor: '#bbb',
+      gridColor: '#666',
+      secGridColor: '#2a2a2a',
+      labelColor: '#59c87b',
+      labelsXEvery: 4,
+      labelsYCount: 4
+    };
+    this.config = Object.assign(this.config, options);
     this.ctx = canvas.getContext('2d');
     this.width = canvas.width;
     this.height = canvas.height;
-    this.config = {
-      padding: 40,
-      lineColor: 'white'
-    }
+    this.innerWidth = canvas.width - this.config.padding * 2;
+    this.innerHeight = canvas.height - this.config.padding * 2;
   }
   // expects data to be array of [[key, val], [key, val]]
   draw(data) {
-    this.drawXScale(data);
-    this.drawYScale(data);
-    this.drawLinePoints(data);
+    this.drawXScale(data, {grid: false});
+    this.drawYScale(data, {grid: true, sep: 2});
+    this.drawLinePoints(data, {labels: false, points: false});
   }
-  drawXScale(data, sep = [5, 10]) {
-    // start horizontal line
-    this.ctx.strokeStyle = 'grey';
+  drawXScale(data, options) {
+    let defaultOptions = {
+      sep: 5,
+      grid: false,
+      gridLineEvery: 2
+    };
+    options = options || {};
+    options = Object.assign(defaultOptions, options);
+    // start axis line
+    this.ctx.strokeStyle = this.config.gridColor;
     this.ctx.lineWidth = 1;
     this.ctx.beginPath();
     this.ctx.moveTo(this.config.padding, this.height - this.config.padding);
     this.ctx.lineTo(this.width - this.config.padding, this.height - this.config.padding);
     this.ctx.stroke();
-    // end horizontal line
+    // end axis line
+    let currX = 0;
+    let lastX = currX;
     data.forEach((d, i) => {
-      if (i === 0) {
-        // skip first to avoid lines in origin
-        return;
+      currX = this.getScaledX(i, data.length - 1);
+      if(!options.grid) {
+        let len = (i % options.sep === 0) ? 10 : 5;
+        len = (i % (options.sep * 2) === 0) ? 15 : len;
+        this.ctx.beginPath();
+        this.ctx.moveTo(currX, this.height - this.config.padding);
+        this.ctx.lineTo(currX, this.height - len - this.config.padding);
+        this.ctx.stroke();
       }
-      let currX = this.getScaledX(i, data.length);
-      let len = (i % sep[0] === 0) ? 10 : 5;
-      if (sep.length > 1) {
-        len = (i % sep[1] === 0) ? 15 : len;
+      // draw labels
+      if (i % this.config.labelsXEvery === 0) {
+        this.drawLabel(d[0], currX, this.height - this.config.padding + 18);
       }
-      this.ctx.beginPath();
-      this.ctx.moveTo(currX, this.height - this.config.padding);
-      this.ctx.lineTo(currX, this.height - len - this.config.padding);
-      this.ctx.stroke();
-      // if (labels && typeof labels[i] !== 'undefined') {
-      //   this.ctx.fillStyle = 'white';
-      //   this.ctx.textAlign = 'center';
-      //   this.ctx.font = "14px Arial";
-      //   this.ctx.fillText(labels[i], currX, this.height - this.config.padding + 20);
-      // }
+      if(options.grid === true) {
+        this.ctx.strokeStyle = this.config.gridColor;
+        if(i % (options.sep * 2) === 0) {
+          this.ctx.strokeStyle = this.config.secGridColor;
+        }
+        this.ctx.beginPath();
+        this.ctx.moveTo(currX, this.height - this.config.padding);
+        this.ctx.lineTo(currX, this.config.padding);
+        this.ctx.stroke();
+      }
+      lastX = currX;
     });
   }
 
-  drawYScale(data, sep = [5, 10]) {
-    // start vertical line
-    this.ctx.strokeStyle = 'grey';
+  drawYScale(data, givenOptions) {
+    let defaultOptions = {
+      sep: 5,
+      grid: false,
+      gridLineEvery: 4
+    };
+    let options = givenOptions || {};
+    options = Object.assign(defaultOptions, options);
+    let max = data.reduce((acc, d) => Math.max(acc, d[1]), data[0][1]);
+    // adjust separators to reasonable size
+    if(givenOptions.sep && givenOptions.sep.length > 0) {
+      options.sep = Math.min(max / 5, options.sep);
+      options.sep = Math.min(max / 2, options.sep);
+    }
+    // start axis line
+    this.ctx.strokeStyle = this.config.gridColor;
     this.ctx.lineWidth = 1;
     this.ctx.beginPath();
     this.ctx.moveTo(this.config.padding, this.height - this.config.padding);
     this.ctx.lineTo(this.config.padding, this.config.padding);
     this.ctx.stroke();
-    // end vertical line
-    let max = data.reduce((acc, d) => Math.max(acc, d[1]), data[0][1]);
-    for(let i = 0; i < max; i += 5) {
-      let currY = this.getScaledY(i, max);
-      this.ctx.beginPath();
-      this.ctx.moveTo(this.config.padding, currY);
-      let len = (i % sep[0] === 0) ? 10 : 5;
-      if (sep.length > 1) {
-        len = (i % sep[1] === 0) ? 15 : len;
+    // end axis line
+    let currY = 0;
+    let lastY = currY;
+    for(let i = 0; i < max; i += this.innerHeight / (this.config.labelsYCount - 1)) {
+      currY = this.getFlippedY(this.getScaledY(i, max));
+      if (!options.grid) {
+        let len = (i % options.sep === 0 && i % options.sep !== 0) ? 10 : 5;
+        len = (i % (options.sep * 2) === 0) ? 15 : len;
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.config.padding, currY);
+        this.ctx.lineTo(this.config.padding + len, currY);
+        this.ctx.stroke();
       }
-      this.ctx.lineTo(this.config.padding + len, currY);
-      this.ctx.stroke();
-      // if (i % 10 === 0) {
-      //   this.ctx.fillStyle = 'white';
-      //   this.ctx.textAlign = 'right';
-      //   this.ctx.font = "14px Arial";
-      //   this.ctx.fillText(data[0][1], this.config.padding - 4, currY);
-      // }
+      lastY = currY;
+    }
+    // draw labels
+    let ystep = this.innerHeight / this.config.labelsYCount;
+    for(let i = 1; i <= this.config.labelsYCount; i++) {
+      let y = ystep * i;
+      let perc = y / this.innerHeight;
+      let closestIndex = this.findClosestToPercentage(data, max, perc);
+      let d = data[closestIndex];
+      let l = d[1];
+      l = Math.round(perc * max * 10) / 10;
+      let flippedY = this.getFlippedY(y) - this.config.padding;
+      this.drawLabel(l + '', this.config.padding - 4, flippedY + 4, 'right');
+      if (options.grid === true) {
+        // this.ctx.strokeStyle = () ? this.config.gridColor;
+        this.ctx.strokeStyle = this.config.gridColor;
+        if(i % options.sep !== 0) {
+          this.ctx.strokeStyle = this.config.secGridColor;
+        }
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.config.padding, flippedY);
+        this.ctx.lineTo(this.width - this.config.padding, flippedY);
+        this.ctx.stroke();
+      }
     }
   }
-  drawLinePoints(data, scale = 10) {
+  drawLinePoints(data, options = {}) {
+    options = Object.assign({
+      labels: false,
+      labelsEvery: 4,
+      radius: 3,
+      points: true
+    }, options);
     this.clear();
     let currX = this.config.padding;
     let max = data.reduce((acc, d) => Math.max(acc, d[1]), 0);
@@ -107,18 +154,46 @@ class CanvasRenderer {
     this.ctx.strokeStyle = this.config.lineColor;
     this.ctx.lineWidth = 1;
     this.ctx.moveTo(currX, this.getFlippedY(this.getScaledY(data[0][1], max)));
-    let step = (this.width - (this.config.padding * 2)) / (data.length - 1);
-    data.forEach(d => {
-      this.ctx.lineTo(currX, this.getFlippedY(this.getScaledY(d[1], max)));
+    let step = this.innerWidth / (data.length - 1);
+    data.forEach((d, i) => {
+      let y = this.getFlippedY(this.getScaledY(d[1], max));
+      this.ctx.lineTo(currX, y);
+      if(options.points === true) {
+        this.ctx.arc(currX, y, options.radius, 0, 2 * Math.PI);
+      }
+      this.ctx.moveTo(currX, y);
+      if(options.labels === true && i % options.labelsEvery === 0) {
+        this.drawLabel(d[1], currX + 6, y - 4, 'left');
+      }
       currX += step;
     });
     this.ctx.stroke();
   }
+  drawLabel(l, x, y, align = 'center') {
+    this.ctx.fillStyle = this.config.labelColor;
+    this.ctx.textAlign = align;
+    this.ctx.font = "14px Arial";
+    this.ctx.fillText(l, x, y);
+  }
+  findClosestToPercentage(data, max, perc) {
+    let closest = 0;
+    let closestPair = data.reduce((acc, d, i) => {
+      let relPerc = d[1] / max;
+      let distance = Math.abs(relPerc - perc);
+      if (distance < acc[0]) {
+        return [distance, i];
+      } else {
+        return acc;
+      }
+    }, [data[0][1], 0]);
+    closest = closestPair[1]
+    return closest;
+  }
   getScaledY(y, max) {
-    return y / max * (this.height - this.config.padding * 2) + this.config.padding;
+    return y / max * this.innerHeight + this.config.padding;
   }
   getScaledX(x, max) {
-    return x / max * (this.width - this.config.padding * 2) + this.config.padding;
+    return x / max * this.innerWidth + this.config.padding;
   }
   getFlippedY(y) {
     return this.height - y;
@@ -134,7 +209,7 @@ const randomData = function(count = 100, max = 200) {
   let startDate = new Date();
   for (let i = 0; i < count; i++) {
     data.push([
-      startDate.ddmmyyyy(), 
+      startDate.ddmmyyyy(),
       Math.round(Math.random() * max)
     ]);
     startDate = addDays(startDate);
@@ -149,16 +224,16 @@ const addDays = function(date, days = 1) {
 }
 
 const start = function() {
-  let data = randomData(13, 50);
+  let data = randomData(100, 130);
   const canvas = document.querySelector('#canvas');
   canvas.width = 1000;
   canvas.height = 450;
-  const cRenderer = new CanvasRenderer(canvas);
+  const cRenderer = new CanvasRenderer(canvas, {
+    labelsXEvery: 20,
+    labelsYCount: 4
+  });
   cRenderer.draw(data);
-  
-  const cont = document.querySelector('.container');
-  const dRenderer = new DivRenderer(cont);
-  // dRenderer.draw(data);
+
 }
 
 start();
